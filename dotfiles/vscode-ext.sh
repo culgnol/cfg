169ff0399aaa6cb784f21be188193ebb57142144
Long Luc <culgnol@users.noreply.github.com>
2020-05-01 23:19:27 -0700

    Create vscode-ext.sh

---
+++ dotfiles/vscode-ext.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#markdown
+code --install-extension joaompinto.asciidoctor-vscode
+
+#themes
+code --install-extension johnpapa.vscode-peacock
+code --install-extension wesbos.theme-cobalt2
